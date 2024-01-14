f57e83381410d75be382d279e0780ad71cae0732
Nikhilpra17 <97670140+Nikhilpra17@users.noreply.github.com>
2024-01-14 23:12:47 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <>
       <Header />
-      <main className="py-3">
+      <main className="py-3" >
         <Container>
           <Outlet />
         </Container>
